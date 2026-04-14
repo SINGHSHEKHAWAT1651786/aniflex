2dae36c4c01869c733e973ff0c474ffa0ed7a86e
Govind Shekhawat <SINGHSHEKHAWAT1651786@GMAIL.COM>
2026-04-14 22:17:56 +0530

    create and update VideoContainer, contants

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 const VideoContainer =() => {
+    useEffect(()=>{
+        getVideos();
+    }, []);
+    const getVideos = async () => {
+        const data = await fetch(YOUTUBE_API_URL);
+        const json = await data.json();
+        console.log();
+
+    };
+    
     return <div>Video Container</div>
     
 };
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,3 +1,4 @@
-const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=10&regionCode=IN&key=YOUR_API_KEY";
 
 const GOOGLE_API_KEY = "AIzaSyCiKWb3mE16ufY5WrWn6m1irLD0GPqKkl0";
+ export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=10&regionCode=IN&key=" + GOOGLE_API_KEY;
+
