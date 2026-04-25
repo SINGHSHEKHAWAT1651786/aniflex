1cfe7bfe0c6b6f68ed0c6ecf0ae371b9a9f499be
Govind Shekhawat <SINGHSHEKHAWAT1651786@GMAIL.COM>
2026-04-25 21:31:07 +0530

    create and update  Head, contants

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,74 +1,114 @@
-
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
-import { YOUTUBE_SEARCH_API } from "../utils/contants";
+import { YOUTUBE_SEARCH_API, GOOGLE_API_KEY } from "../utils/contants";
 
 const Head = () => {
-    const [searchQuery, setSearchQuery] = useState("");
-    useEffect(()=> {
-//APIs Call
-console.log(searchQuery);
-//make an apis call after key press
-// but if the difference between 2 APIs call <200ms 
-//decline the APIs call
-   const timer = setTimeout(()=>getSearchSuggestions(), 200);
-       return () => {
-clearTimeout(timer);
-       };
-    }, [searchQuery]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
-    /** 
-     * key -i
-     * - render the component
-     * - useEffect();
-     * - start timer => make apis call after 200ms
-     *   
-     * key-ip
-     * -destory the component, also call return useffect
-     * - re-render the component
-     * - useEffect();
-     * - start timer => new timer => make apis call after 200ms 
-    */
-    const getSearchSuggestions = async () => {
-        console.log(searchQuery);
-        const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
-        const json = await data.json();
-        console.log(json[1]); 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!searchQuery)  {
+      setSuggestions([]);
+      return;
     }
-    const dispatch = useDispatch();
 
-    const toggleMenuHandler = () => {
-        dispatch(toggleMenu());
-    };
-    return(
-        <div className="grid grid-flow-col p-4 m-2 shadow-lg cursor-pointer">
-            
-            <div className="flex col-span-1">
-                <i onClick={() => toggleMenuHandler()} 
-                className="fa-solid fa-bars text-2xl"></i>
-                <a href="/">
-                <img 
-                    alt='logo' 
-                    className="w-10 h-8 mx-2"
-                    src='C:\Users\dell\Desktop\Aniflex\aniflex\public\logo192.png'
-                />
-                </a>
-            </div>
-       
-            <div className="col-span-10 px-10">
-                <input type="text" className="px-2 w-1/2 border border-gray-400 p-2 rounded-l-full" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
-                <button className="border border-gray-400 px-5 p-2 rounded-r-full bg-gray-100 ">
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                </button>
-            </div> 
-       
-            <div className="col-span-1">
-                <i className="fa-solid fa-user text-2xl"></i>
-            </div>
-   
+    const timer = setTimeout(() => {
+      getSearchSuggestions();
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    try {
+      const data = await fetch(
+        YOUTUBE_SEARCH_API + searchQuery + "&key=" + GOOGLE_API_KEY
+      );
+
+      const json = await data.json();
+
+      setSuggestions(json?.items || []);
+    } catch (error) {
+      console.error("API Error:", error);
+    }
+  };
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
+  return (
+    <div className="grid grid-flow-col p-4 m-2 shadow-lg">
+      
+      {/* LEFT SECTION */}
+      <div className="flex col-span-1 items-center">
+        <i
+          onClick={toggleMenuHandler}
+          className="fa-solid fa-bars text-2xl cursor-pointer"
+        ></i>
+
+        <a href="/">
+          <img
+            alt="logo"
+            className="w-10 h-8 mx-2"
+            src="/logo192.png"
+          />
+        </a>
+      </div>
+
+      {/* SEARCH SECTION */}
+      <div className="col-span-10 px-10 relative">
+        <div>
+          <input
+            type="text"
+            className="px-3 w-1/2 border border-gray-400 p-2 rounded-l-full"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+ SHOW
+            onFocus={() => setShowSuggestions(true)}
+
+            // HIDE 
+            onBlur={() => setShowSuggestions(false)}
+
+            placeholder="Search..."
+          />
+
+          <button className="border border-gray-400 px-5 p-2 rounded-r-full bg-gray-100">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
         </div>
-    );
+
+        {/*  Suggestions Dropdown */}
+        {showSuggestions && suggestions.length > 0 && (
+          <div className="absolute bg-white py-2 px-5 w-1/2 shadow-lg rounded-lg">
+            <ul>
+              {suggestions.map((item) => (
+                <li
+                  key={item?.etag} // ✅ safer unique key
+                  className="flex items-center gap-3 py-2 px-2 hover:bg-gray-100 cursor-pointer"
+                >
+                  <i className="fa-solid fa-magnifying-glass text-gray-500"></i>
+
+                  <span className="text-sm">
+                    {item?.snippet?.title}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
+      {/* RIGHT SECTION */}
+      <div className="col-span-1 flex items-center justify-end">
+        <i className="fa-solid fa-user text-2xl"></i>
+      </div>
+    </div>
+  );
 };
 
 export default Head;
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,4 +1,7 @@
+export const GOOGLE_API_KEY = "AIzaSyDDpPQe3zItVXKQGD1eLFOTC6SP8hQ4RhU";
 
-const GOOGLE_API_KEY = "AIzaSyDDpPQe3zItVXKQGD1eLFOTC6SP8hQ4RhU";
- export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&q=";
+export const YOUTUBE_VIDEOS_API =
+  `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
+
+export const YOUTUBE_SEARCH_API =
+  `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=`;
